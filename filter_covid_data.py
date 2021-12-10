892c97b47ba1b52d17eb53b2e9d368e0e33ea434
Victor Zheng <zhengvictor0@gmail.com>
2021-12-10 00:41:26 -0500

    added comments to the filter covid and filter employment files

--- filter_covid_data.py
+++ filter_covid_data.py
@@ -1,23 +1,33 @@
-"""File to filter covid data"""
+"""Filter covid data from the original dataset covid_data.csv and produce a new file called
+filtered_covid_cases.csv data with the date and the number of cases"""
 
 import csv
 
 filtered_data = {}
+# read the covid_data.csv file. This file is too large to submit or put onto GitHub.
 with open('covid_data.csv') as csv_file:
+    # read through each line in covid_data.csv
     csv_reader = csv.reader(csv_file, delimiter=',')
+    # skip the first line with variable names
     next(csv_reader)
-
+    # open filtered_covid_data.csv to write
     file = open('filtered_covid_data.csv', 'w')
+    # initialize built in csv writer method to write to file
     writer = csv.writer(file)
-
+    # iterate through each row in the file
     for row in csv_reader:
+        # find the "Accurate_Episode_Date" variable and record the date
         date = row[1][0:7]
+        # check if the date is in the dictionary currently
         if date not in filtered_data:
+            # if date is not currently in dictionary, initialize the date
             filtered_data[date] = 0
-
+        # increase the accumulator of covid cases for that month
         filtered_data[date] += 1
 
+    # iterate through each date in and write to file the date and the number of covid cases
     for date in filtered_data:
         writer.writerow([date, filtered_data[date]])
 
+    # close the file
     file.close()
--- filter_employment_data.py
+++ filter_employment_data.py
@@ -1,23 +1,31 @@
-"""Filtered employment_data.csv"""
+"""Filter employment data from the original dataset employment_data.csv and produce a new
+file with the filtered data called filtered_employment_data.csv containing the industry and number
+of employed per month"""
 
 import csv
 
-new_lst = []
+row_list = []
 new_dict = {}
 
+# open the employment data csv file
 with open('employment_data.csv') as f:
     reader = csv.reader(f, delimiter=',')
-
+    # skip the first 13 lines of the code
     [next(reader, None) for item in range(13)]
-
+    # iterate through each row in the reader
     for row in reader:
-        new_lst.append(row)
+        # add each row to the row_list
+        row_list.append(row)
 
-    content = new_lst[0: 18]
+    # filter only the first 19 rows that we need
+    content = row_list[0: 18]
+    # open filtered_employment_data.csv to write the filtered data
     file = open('filtered_employment_data.csv', 'w')
     writer = csv.writer(file)
+    # iterate through each element in lst and write the content
     for lst in content:
-        row = lst
-        print(row)
-        writer.writerow(row)
+        print(lst)
+        writer.writerow(lst)
+
+    # close the file
     file.close()
