a3e8c1fe919b42fad13c478ace11145f9eb6511b
Victor Zheng <zhengvictor0@gmail.com>
2021-12-10 00:23:30 -0500

    created functions to add covid and employment data

--- code_data.py
+++ code_data.py
@@ -27,29 +27,40 @@ class CovidData:
     cases: int
 
 
-employment_list = []
-with open('filtered_employment_data.csv') as f:
-    # read the entire csv file
-    reader = csv.reader(f, delimiter=',')
-    # iterate through each line in the reader
-    for row in reader:
-        # check if the line is not empty
-        if row != []:
-            filter_data = []  # create a list to store the employment numbers
-            for i in range(1, len(row)):  # iterate through the employment numbers
-                # append to filter data with correct format
-                filter_data.append(float(row[i].replace(',', '')))
-            # add the Employed object to the employment list
-            employment_list.append(Employed(industry=row[0], employment=filter_data))
+def add_employment_data() -> list[Employed]:
+    """Return employment data in a list containing Employment objects read from
+    filtered_employment_data.csv
+    """
+    employment_list = []
+    with open('filtered_employment_data.csv') as f:
+        # read the entire csv file
+        reader = csv.reader(f, delimiter=',')
+        # iterate through each line in the reader
+        for row in reader:
+            # check if the line is not empty
+            if row != []:
+                filter_data = []  # create a list to store the employment numbers
+                for i in range(1, len(row)):  # iterate through the employment numbers
+                    # append to filter data with correct format
+                    filter_data.append(float(row[i].replace(',', '')))
+                # add the Employed object to the employment list
+                employment_list.append(Employed(industry=row[0], employment=filter_data))
+
+    return employment_list
 
 
-covid_data = []
-with open('filtered_covid_data.csv') as csv_file:
-    # read the entire csv file
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    # iterate through each line in the reader
-    for row in csv_reader:
-        # check if the row is not empty
-        if row != []:
-            # append the covid_data to the CovidData data class
-            covid_data.append(CovidData(date=row[0], cases=int(row[1])))
+def add_covid_data() -> list[CovidData]:
+    """Add covid data to a list containing CovidData objects read from filtered_covid_data.csv"""
+    covid_data = []
+    with open('filtered_covid_data.csv') as csv_file:
+        # read the entire csv file
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        # iterate through each line in the reader
+        for row in csv_reader:
+            # check if the row is not empty
+            if row != []:
+                # append the covid_data to the CovidData data class
+                covid_data.append(CovidData(date=row[0], cases=int(row[1])))
+
+    return covid_data
+
