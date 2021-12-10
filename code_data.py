d5ebd9a0d639aaf7bca09b8f0a8192b61739e95f
Victor Zheng <zhengvictor0@gmail.com>
2021-12-10 00:18:26 -0500

    added comments and updated variable names

--- code_data.py
+++ code_data.py
@@ -4,7 +4,7 @@ import csv
 
 
 @dataclass
-class Employeed:
+class Employed:
     """Employment numbers every month
 
     Instance Attributes:
@@ -27,21 +27,29 @@ class CovidData:
     cases: int
 
 
-e = []
+employment_list = []
 with open('filtered_employment_data.csv') as f:
+    # read the entire csv file
     reader = csv.reader(f, delimiter=',')
+    # iterate through each line in the reader
     for row in reader:
+        # check if the line is not empty
         if row != []:
-            filter_data = []
-            for i in range(1, len(row)):
+            filter_data = []  # create a list to store the employment numbers
+            for i in range(1, len(row)):  # iterate through the employment numbers
+                # append to filter data with correct format
                 filter_data.append(float(row[i].replace(',', '')))
-            object = Employeed(industry=row[0], employment=filter_data)
-            e.append(object)
+            # add the Employed object to the employment list
+            employment_list.append(Employed(industry=row[0], employment=filter_data))
+
 
 covid_data = []
 with open('filtered_covid_data.csv') as csv_file:
+    # read the entire csv file
     csv_reader = csv.reader(csv_file, delimiter=',')
-
+    # iterate through each line in the reader
     for row in csv_reader:
+        # check if the row is not empty
         if row != []:
+            # append the covid_data to the CovidData data class
             covid_data.append(CovidData(date=row[0], cases=int(row[1])))
