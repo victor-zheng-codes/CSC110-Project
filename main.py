e6f39994ede3168e0a4833f3ce58360e0e2f4085
Mapsee <mapsee26@gmail.com>
2021-12-11 23:35:52 -0500

    docs

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ Run this file to run the program
 Copyright and Usage Information
 ===============================
 
-This file is provided solely for the personal and private use of TA and professors
-teaching CSC110 at the University of Toronto St. George campus. All forms of
+This file is provided solely for the personal and private use of students, TA, and professors
+within the CSC110 at the University of Toronto St. George campus. All forms of
 distribution of this code, whether as given or with any changes, are
 expressly prohibited.
 
-This file is Copyright (c) 2021 ...
+This file is Copyright (c) 2021 Daniel Xu, Nicole Leung, Kirsten Sutantyo, and Victor Zheng.
 """
 
 if __name__ == '__main__':
