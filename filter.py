159a60e4babf524762a59ab44dce9bac63214ca0
Dani3lx <1xudan@hdsb.ca>
2021-12-12 20:57:26 -0500

    PythonTA

--- filter.py
+++ filter.py
@@ -15,33 +15,30 @@ def filter_covid() -> None:
         # skip the first line with variable names
         next(csv_reader)
         # open filtered_covid_data.csv to write
-        file = open('filtered_covid_data.csv', 'w')
-        # initialize built in csv writer method to write to file
-        writer = csv.writer(file)
-        # iterate through each row in the file
-        for row in csv_reader:
-            # find the "Accurate_Episode_Date" variable and record the date
-            date = row[1][0:7]
-            # check if the date is in the dictionary currently
-            if date not in filtered_data:
-                # if date is not currently in dictionary, initialize the date
-                filtered_data[date] = 0
-            # increase the accumulator of covid cases for that month
-            filtered_data[date] += 1
+        with open('filtered_covid_data.csv', 'w') as file:
+            # initialize built in csv writer method to write to file
+            writer = csv.writer(file)
+            # iterate through each row in the file
+            for row in csv_reader:
+                # find the "Accurate_Episode_Date" variable and record the date
+                date = row[1][0:7]
+                # check if the date is in the dictionary currently
+                if date not in filtered_data:
+                    # if date is not currently in dictionary, initialize the date
+                    filtered_data[date] = 0
+                # increase the accumulator of covid cases for that month
+                filtered_data[date] += 1
 
-        writer.writerow(('Date', 'Number of COVID cases'))
-        # iterate through each date in and write to file the date and the number of covid cases
-        for date in filtered_data:
-            writer.writerow([date, filtered_data[date]])
-
-        # close the file
-        file.close()
+            writer.writerow(('Date', 'Number of COVID cases'))
+            # iterate through each date in and write to file the date and the number of covid cases
+            for date in filtered_data:
+                writer.writerow([date, filtered_data[date]])
 
 
 def filter_employment_data() -> None:
     """Filter employment data from the original dataset employment_data.csv and produce a new
-    file with the filtered data called filtered_employment_data.csv containing the industry and number
-    of employed per month"""
+    file with the filtered data called filtered_employment_data.csv containing the industry and
+    number of employed per month"""
 
     row_list = []
 
@@ -49,52 +46,44 @@ def filter_employment_data() -> None:
     with open('employment_data.csv') as f:
         reader = csv.reader(f, delimiter=',')
         # skip the first 13 lines of the code
-        [next(reader, None) for item in range(11)]
-
-        # f = open('data/employment_months.csv', 'w')
-        # writer = csv.writer(f)
+        _ = [next(reader, None) for _ in range(11)]
 
         # open filtered_employment_data.csv to write the filtered data
-        file = open('filtered_employment_data.csv', 'w')
-        writer = csv.writer(file)
+        with open('filtered_employment_data.csv', 'w') as file:
+            writer = csv.writer(file)
 
-        header_row = next(reader)
-        items = [item for item in header_row[1:]]
-        print(items)
+            header_row = next(reader)
+            items = header_row[1:]
 
-        # convert calendar to the correct format
-        converted_cal = convert_date_format(items)
-        # insert to the beginning of the header an industry descriptor
-        converted_cal.insert(0, 'Industry')
-        writer.writerow(converted_cal)
+            # convert calendar to the correct format
+            converted_cal = convert_date_format(items)
+            # insert to the beginning of the header an industry descriptor
+            converted_cal.insert(0, 'Industry')
+            writer.writerow(converted_cal)
 
-        next(reader, None)
+            next(reader, None)
 
-        # iterate through each row in the reader
-        for row in reader:
-            row_list.append(row)
+            # iterate through each row in the reader
+            for row in reader:
+                row_list.append(row)
 
-        # filter only the first 19 rows that we need
-        content = row_list[0: 18]
+            # filter only the first 19 rows that we need
+            content = row_list[0: 18]
 
-        # iterate through each element in lst and write the content
-        for lst in content:
-            # remove numeric numbers at the end
-            list_el = lst[0].split()
-            if list_el[-1].isnumeric():
-                list_el.pop()
-                lst[0] = ' '.join(list_el)
+            # iterate through each element in lst and write the content
+            for lst in content:
+                # remove numeric numbers at the end
+                list_el = lst[0].split()
+                if list_el[-1].isnumeric():
+                    list_el.pop()
+                    lst[0] = ' '.join(list_el)
 
-            # for some reason, some industries have more than 1 trailing digit
-            if list_el[-1].isnumeric():
-                list_el.pop()
-                lst[0] = ' '.join(list_el)
+                # for some reason, some industries have more than 1 trailing digit
+                if list_el[-1].isnumeric():
+                    list_el.pop()
+                    lst[0] = ' '.join(list_el)
 
-            print(lst)
-            writer.writerow(lst)
-
-        # close the file
-        file.close()
+                writer.writerow(lst)
 
 
 def convert_date_format(input_dates: list[str]) -> list[str]:
@@ -104,8 +93,8 @@ def convert_date_format(input_dates: list[str]) -> list[str]:
     ['2020-12', '2021-04']
     """
 
-    calendar = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
-                'November', 'December']
+    calendar = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+                'September', 'October', 'November', 'December']
 
     # add a csv header with all months
     new_cal = []
@@ -119,3 +108,18 @@ def convert_date_format(input_dates: list[str]) -> list[str]:
                 new_cal.append(y + '-' + str(calendar.index(m) + 1))
 
     return new_cal
+
+
+if __name__ == '__main__':
+    import python_ta
+    import python_ta.contracts
+
+    python_ta.contracts.DEBUG_CONTRACTS = False
+    python_ta.contracts.check_all_contracts()
+
+    python_ta.check_all(config={
+        'allowed-io': ['filter_covid', 'filter_employment_data, convert_date_format'],
+        'extra-imports': ['python_ta.contracts', 'csv'],
+        'max-line-length': 100,
+        'disable': ['R1705', 'C0200']
+    })
